50d6e19e0f6d44b6cadb6bd110077815440c37b8
BlackShark <Bl4ckSh4rk@users.noreply.github.com>
2023-08-23 23:52:03 +0200

    Use filtered Item source

--- WC3Plugin/RM3Form.cs
+++ WC3Plugin/RM3Form.cs
@@ -6,7 +6,7 @@ public partial class RM3Form : Form
 {
     private readonly SAV3 sav;
 
-    private const ushort MAX_ITEM = 0x178;
+    private const ushort EONTICKET = 0x113;
 
     public RM3Form(SAV3 sav)
     {
@@ -14,19 +14,24 @@ public partial class RM3Form : Form
 
         InitializeComponent();
 
-        ItemComboBox.DataSource = GameInfo.Strings.GetItemStrings(sav.Context, sav.Version).ToArray();
+        List<ComboItem> items = new();
+        items.AddRange(GameInfo.FilteredSources.Items);
+        items.Add(new(GameInfo.Strings.GetItemStrings(sav.Context, sav.Version)[EONTICKET], EONTICKET));
+        ItemComboBox.DataSource = new BindingSource(items, null);
         ItemComboBox.DisplayMember = "Text";
+        ItemComboBox.ValueMember = "Value";
 
-        ItemComboBox.SelectedIndex = ((IGen3Hoenn)sav).RecordMixingGift.Item > MAX_ITEM ? 0 : ((IGen3Hoenn)sav).RecordMixingGift.Item;
+        ItemComboBox.SelectedValue = IsValidItem(((IGen3Hoenn)sav).RecordMixingGift.Item) ? ((IGen3Hoenn)sav).RecordMixingGift.Item : 0;
         CountBox.Value = ((IGen3Hoenn)sav).RecordMixingGift.Count;
     }
 
     private void SaveButton_Click(object sender, EventArgs e)
     {
+        ushort selected = (ushort)((int)ItemComboBox.SelectedValue! & 0xFFFF);
         RecordMixing3Gift rm3 = new(((IGen3Hoenn)sav).RecordMixingGift.Data)
         {
-            Item = (ushort)ItemComboBox.SelectedIndex,
-            Count = (byte)(ItemComboBox.SelectedIndex < 1 ? 0 : CountBox.Value),
+            Item = selected,
+            Count = (byte)(selected == 0 ? 0 : CountBox.Value),
             Max = 1 // unused
         };
         rm3.FixChecksum();
@@ -36,13 +41,28 @@ public partial class RM3Form : Form
 
     private void ItemComboBox_SelectedIndexChanged(object sender, EventArgs e)
     {
-        if (ItemComboBox.SelectedIndex <= 0)
+        ushort selected = (ushort)((int)ItemComboBox.SelectedValue! & 0xFFFF);
+        if (selected == 0)
         {
             CountBox.Value = 0;
         }
-        else if (ItemComboBox.SelectedIndex > 0 && CountBox.Value == 0)
+        else if (selected != 0 && CountBox.Value == 0)
         {
             CountBox.Value = 1;
         }
+    }
+
+    private bool IsValidItem(ushort item)
+    {
+        if (item is 0 or EONTICKET)
+            return true;
+
+        IItemStorage storage;
+        if (sav is SAV3E)
+            storage = new ItemStorage3E();
+        else
+            storage = new ItemStorage3RS();
+
+        return storage.GetItems(InventoryType.PCItems).Contains(item);
     }
 }
